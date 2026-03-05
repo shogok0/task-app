872d0d2e582d745ab43bb3078a671fb1883e82a5
shogok0 <shogo.k.0312@icloud.com>
2026-03-05 15:23:23 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+app = Flask(__name__)
+
+# データベース初期化
+def init_db():
+    conn = sqlite3.connect("tasks.db")
+    c = conn.cursor()
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task TEXT,
+        deadline TEXT
+    )
+    """)
+    conn.commit()
+    conn.close()
+
+init_db()
+
+
+@app.route("/")
+def index():
+    conn = sqlite3.connect("tasks.db")
+    c = conn.cursor()
+    c.execute("SELECT task, deadline FROM tasks")
+    tasks = [{"task":row[0], "deadline":row[1]} for row in c.fetchall()]
+    conn.close()
+    return render_template("index.html", tasks=tasks)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    task = request.form["task"]
+    deadline = request.form["deadline"]
+
+    conn = sqlite3.connect("tasks.db")
+    c = conn.cursor()
+    c.execute("INSERT INTO tasks (task, deadline) VALUES (?,?)",(task,deadline))
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+@app.route("/delete/<int:task_id>")
+def delete(task_id):
+
+    conn = sqlite3.connect("tasks.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM tasks WHERE id=?", (task_id,))
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
+
+from flask import send_from_directory
+@app.route("/manifest.json")
+def manifest():
+    return send_from_directory(".", "manifest.json")
